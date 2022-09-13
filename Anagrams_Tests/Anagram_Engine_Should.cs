4303e970eba283af59e8ba22340700f0ed8d2c99
Dave Bage <david_bage@hotmail.com>
2022-09-13 15:35:45 +0100

    Refactored unit tests

--- Anagrams_Tests/Anagram_Engine_Should.cs
+++ Anagrams_Tests/Anagram_Engine_Should.cs
@@ -8,69 +8,19 @@ namespace Anagrams_Tests
 {
     public class Anagram_Engine_Should
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        [TestCase("A")]
-        [TestCase("B")]
-        [TestCase("C")]
-        public void deduce_character_permutations_of_anagram(string anagram)
-        {
-            AnagramEngine _anagramEngine = new AnagramEngine();
-
-            Assert.That(_anagramEngine.GetPermutations(anagram).First(), Is.EqualTo(anagram));
-        }
-
-        private static List<string> sdsdsdsd(params string[] words)
-        {
-            return words.ToList();
-        }
-
-        [Test]
+        [TestCase("A", new string[] { "A" })]
+        [TestCase("B", new string[] { "B" })]
+        [TestCase("C", new string[] { "C" })]
         [TestCase("AB", new string[] { "AB", "BA" })]
         [TestCase("CD", new string[] { "CD", "DC" })]
         [TestCase("EF", new string[] { "EF", "FE" })]
-        [TestCase("GH", new string[] { "GH", "HG" })]
-        public void deduce_character_permutations_of_two_character_anagram(string anagram, string[] expectedResults)
-        {
-            AnagramEngine _anagramEngine = new AnagramEngine();
-
-            List<string> permutations = _anagramEngine.GetPermutations(anagram);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(permutations.Count, Is.EqualTo(expectedResults.Length));
-                Assert.That(permutations, Is.EquivalentTo(expectedResults));
-            });
-
-        }
-
-
-        [Test]
         [TestCase("ABC", new string[] { "ABC", "ACB", "BAC", "BCA", "CAB", "CBA" })]
         [TestCase("DEF", new string[] { "DEF", "DFE", "EDF", "EFD", "FED", "FDE" })]
         [TestCase("GOT", new string[] { "GOT", "GTO", "OGT", "OTG", "TGO", "TOG" })]
-        public void deduce_character_permutations_of_three_character_anagram(string anagram, string[] expectedResults)
-        {
-            AnagramEngine _anagramEngine = new AnagramEngine();
-
-            List<string> permutations = _anagramEngine.GetPermutations(anagram);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(permutations.Count, Is.EqualTo(expectedResults.Length));
-                Assert.That(permutations, Is.EquivalentTo(expectedResults));
-            });
-
-        }
-
-        [Test]
         [TestCase("ABCD", new string[] { "ABCD", "ABDC", "ACDB", "ACBD", "ADBC", "ADCB", "BACD", "BADC", "BCAD", "BCDA", "BDAC", "BDCA", "CABD", "CADB", "CBAD", "CBDA", "CDAB", "CDBA", "DABC", "DACB", "DBAC", "DBCA", "DCAB", "DCBA" })]
         [TestCase("BIRO", new string[] { "BIRO", "BIOR", "BRIO", "BROI", "BORI", "BOIR", "IBRO", "IBOR", "IOBR", "IORB", "IRBO", "IROB", "RBIO", "RBOI", "RIBO", "RIOB", "ROBI", "ROIB", "OBIR", "OBRI", "OIBR", "OIRB", "ORBI", "ORIB" })]
-        public void deduce_character_permutations_of_four_character_anagram(string anagram, string[] expectedResults)
+        public void deduce_character_permutations_of_anagram(string anagram, string[] expectedResults)
         {
             AnagramEngine _anagramEngine = new AnagramEngine();
 
@@ -83,6 +33,5 @@ namespace Anagrams_Tests
             });
 
         }
-
     }
 }
